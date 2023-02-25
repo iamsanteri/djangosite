48388716d64ffbba2e325ed5c73002d86e022e09
Santeri Liukkonen <santeri@Santeris-MacBook-Air.local>
2023-02-25 21:03:51 +0200

    Built out commenting and read later features

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts/", views.posts, name="all_posts"),
-    path("posts/<slug:slug>", views.post, name="single_post")
+    path("", views.StartingPageView.as_view(), name="index"),
+    path("posts/", views.AllPostsView.as_view(), name="all_posts"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="single_post"),
+    path("read-later", views.ReadLaterView.as_view(), name="read_later")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,115 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse
+from django.views import View
+from django.http import HttpResponseRedirect
 
 from .models import Post
+from .forms import CommentForm
 
 # Temporary utility function
 def get_date(post):
     return post['date']
 
 # Create your views here.
-def index(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-       "all_posts": all_posts
-    })
+class StartingPageView(ListView):
+    template_name ="blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def post(request, slug):
-    try:
-        identified_post = get_object_or_404(Post, slug=slug)
-        return render(request, "blog/single-post.html", {
-            "post": identified_post,
-            "post_tags": identified_post.tags.all()
-        })
-    except:
-        raise Http404()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+
+class SinglePostView(View):
+
+    def is_stored_post(self, request, post_id): 
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else: 
+            is_saved_for_later = False
+        
+        return is_saved_for_later
+
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/single-post.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+
+            # Create an instance of comment instead of saving directly 
+            # This is done for extra processing before saving to DB
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("single_post", args=[slug]))
+        
+        else: 
+            context = {
+                "post": post,
+                "post_tags": post.tags.all(),
+                "comment_form": comment_form,
+                "comments": post.comments.all().order_by("-id"),
+                "saved_for_later": self.is_stored_post(request, post.id)
+            }
+
+            return render(request, "blog/single-post.html", context)
+    
+class ReadLaterView(View):
+    def get(self, request): 
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None: 
+            stored_posts = []
+        
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts: 
+            stored_posts.append(post_id)
+        else: 
+            stored_posts.remove(post_id)
+        
+        request.session["stored_posts"] = stored_posts
+        
+        return HttpResponseRedirect("/")
